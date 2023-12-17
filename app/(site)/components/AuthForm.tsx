861e75b9f13d954cb173e567740fd0e4b9b758d7
Kutoriushio <328752344@qq.com>
2023-12-17 01:19:56 -0500

    Setup MongoDb and Prisma

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -49,83 +49,91 @@ const AuthForm = () => {
   };
 
   return (
-    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-          {variant === "REGISTER" && (
+    <>
+      <h2 className="text-3xl text-center mt-6 font-bold text-gray-900 tracking-tight">
+        {variant === "LOGIN"
+          ? "Sign in to your account"
+          : "Create a new account"}
+      </h2>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            {variant === "REGISTER" && (
+              <Input
+                id="name"
+                label="name"
+                register={register}
+                disabled={isLoading}
+                required
+                errors={errors}
+              />
+            )}
             <Input
-              id="name"
-              label="name"
+              id="email"
+              label="Email address"
               register={register}
               disabled={isLoading}
               required
               errors={errors}
+              type="email"
             />
-          )}
-          <Input
-            id="email"
-            label="Email address"
-            register={register}
-            disabled={isLoading}
-            required
-            errors={errors}
-            type="email"
-          />
-          <Input
-            id="password"
-            label="Password"
-            register={register}
-            disabled={isLoading}
-            required
-            errors={errors}
-            type="password"
-          />
-          <div>
-            <Button disabled={isLoading} fullWidth type="submit">
-              {variant === "LOGIN" ? "Sign in" : "Register"}
-            </Button>
-          </div>
-        </form>
-        <div className="mt-6">
-          <div className="relative">
-            <div className="flex items-center absolute inset-0">
-              <div className="w-full border-t border-gray-300" />
+            <Input
+              id="password"
+              label="Password"
+              register={register}
+              disabled={isLoading}
+              required
+              errors={errors}
+              type="password"
+            />
+            <div>
+              <Button disabled={isLoading} fullWidth type="submit">
+                {variant === "LOGIN" ? "Sign in" : "Register"}
+              </Button>
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">
-                Or continue with
-              </span>
+          </form>
+          <div className="mt-6">
+            <div className="relative">
+              <div className="flex items-center absolute inset-0">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="bg-white px-2 text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="mt-6 flex gap-2">
+              <AuthSocialButton
+                icon={BsGithub}
+                onClick={() => socialAction("github")}
+              />
+              <AuthSocialButton
+                icon={BsGoogle}
+                onClick={() => {
+                  socialAction("google");
+                }}
+              />
             </div>
           </div>
-          <div className="mt-6 flex gap-2">
-            <AuthSocialButton
-              icon={BsGithub}
-              onClick={() => socialAction("github")}
-            />
-            <AuthSocialButton
-              icon={BsGoogle}
-              onClick={() => {
-                socialAction("google");
-              }}
-            />
-          </div>
-        </div>
 
-        <div className="mt-6 text-gray-500 text-sm flex gap-2 justify-center px-2">
-          <div>
-            {variant === "LOGIN"
-              ? "New to MelonChat?"
-              : "Already have an account?"}
-          </div>
-          <div
-            onClick={toggleVariant}
-            className="text-sky-600 cursor-pointer hover:underline hover:text-sky-700"
-          >
-            {variant === "LOGIN" ? "Create an account" : "Login"}
+          <div className="mt-6 text-gray-500 text-sm flex gap-2 justify-center px-2">
+            <div>
+              {variant === "LOGIN"
+                ? "New to MelonChat?"
+                : "Already have an account?"}
+            </div>
+            <div
+              onClick={toggleVariant}
+              className="text-sky-600 cursor-pointer hover:underline hover:text-sky-700"
+            >
+              {variant === "LOGIN" ? "Create an account" : "Login"}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-full bg-gray-100 justify-center py-12 sm:px-6 lg:px-8">
+    <div className="flex flex-col min-h-full bg-gray-100 justify-center py-12 sm:px-6 lg:px-8 px-4">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           src="/images/logo.png"
@@ -12,9 +12,6 @@ export default function Home() {
           height={48}
           className="mx-auto w-auto"
         />
-        <h2 className="text-3xl text-center mt-6 font-bold text-gray-900 tracking-tight">
-          Sign in to your account
-        </h2>
       </div>
       {/* Auth Form */}
       <AuthForm />
