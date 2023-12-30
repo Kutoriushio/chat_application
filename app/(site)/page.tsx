051768f19470be78276cc273566d079dcb8dd1bc
Kutoriushio <328752344@qq.com>
2023-12-30 18:47:59 -0500

    Change the logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,14 +4,8 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-full bg-gray-100 justify-center py-12 sm:px-6 lg:px-8 px-4">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          src="/images/logo.png"
-          alt="logo"
-          width={48}
-          height={48}
-          className="mx-auto w-auto"
-        />
+      <div className="flex justify-center items-center">
+        <Image src="/images/logo.png" alt="logo" width={64} height={64} />
       </div>
       {/* Auth Form */}
       <AuthForm />
